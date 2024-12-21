fd32c8ae1dec23a255bf5130907b59fd276d19ba
Stef Tervelde <stef@steftervelde.nl>
2024-12-21 13:56:12 +0100

    Tabs!

--- src/main/kotlin/p5jsInputHandler.kt
+++ src/main/kotlin/p5jsInputHandler.kt
@@ -7,5 +7,6 @@ import java.awt.event.ActionListener
 class p5jsInputHandler(editor: p5jsEditor): PdeInputHandler(editor) {
     init{
         this.addKeyBinding("ENTER", InputHandler.INSERT_BREAK)
+        this.addKeyBinding("TAB", InputHandler.INSERT_TAB)
     }
 }
